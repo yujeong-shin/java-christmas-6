6789953cf5a73fdec211cafb934ee87ac5f1e3f5
yujeong-shin <vouobb@naver.com>
2023-11-14 22:36:32 +0900

    Feat : 입력 기능 추가
    식당 예상 방문 날짜, 주문 메뉴와 개수를 입력받는 기능을 추가함

---
+++ src/main/java/christmas/InputView.java
@@ -0,0 +1,14 @@
+package christmas;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public int readDate(){
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Integer.parseInt(Console.readLine());
+    }
+    public String readOrder(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return Console.readLine();
+    }
+}
